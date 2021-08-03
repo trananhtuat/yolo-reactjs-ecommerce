d547f345d0d4af909f5929161c39427a291596eb
Tran Anh Tuat <tuattrananh@gmail.com>
2021-08-03 20:33:43 +0700

    product filter

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState, useCallback, useEffect} from 'react'
 
 import Helmet from '../components/Helmet'
 import Grid from '../components/Grid'
 import ProductCard from '../components/ProductCard'
 import CheckBox from '../components/CheckBox'
+import Button from '../components/Button'
 
 import productData from '../assets/fake-data/products'
 import category from '../assets/fake-data/category'
@@ -11,8 +12,92 @@ import colors from '../assets/fake-data/product-color'
 import size from '../assets/fake-data/product-size'
 
 const Catalog = () => {
+
+    const initFilter = {
+        category: [],
+        color: [],
+        size: []
+    }
+
+    const [filter, setFilter] = useState(initFilter)
+
+    const filterSelect = (type, checked, item) => {
+        if (checked) {
+            switch(type) {
+                case "CATEGORY":
+                    setFilter({...filter, category: [...filter.category, item.categorySlug]})
+                    break
+                case "COLOR":
+                    setFilter({...filter, color: [...filter.color, item.color]})
+                    break
+                case "SIZE":
+                    setFilter({...filter, size: [...filter.size, item.size]})
+                    break
+                default:
+                    break
+            }
+        } else {
+            switch(type) {
+                case "CATEGORY":
+                    const newCategory = filter.category.filter(e => e !== item.categorySlug)
+                    setFilter({...filter, category: newCategory})
+                    break
+                case "COLOR":
+                    const newColor = filter.color.filter(e => e !== item.color)
+                    setFilter({...filter, color: newColor})
+                    break
+                case "SIZE":
+                    const newSize = filter.size.filter(e => e !== item.size)
+                    setFilter({...filter, size: newSize})
+                    break
+                default:
+                    break
+            }
+        }
+    }
+
+    const clearFilter = () => setFilter(initFilter)
+
+    const productList = productData.getAllProducts()
+
+    const [products, setProducts] = useState(productList)
+
+    const updateProducts = useCallback(
+        () => {
+            let temp = productList
+
+            if (filter.category.length > 0) {
+                temp = temp.filter(e => filter.category.includes(e.categorySlug))
+            }
+
+            if (filter.color.length > 0) {
+                temp = temp.filter(e => {
+                    const check = e.colors.find(color => filter.color.includes(color))
+                    return check !== undefined
+                })
+            }
+
+            if (filter.size.length > 0) {
+                temp = temp.filter(e => {
+                    const check = e.size.find(size => filter.size.includes(size))
+                    return check !== undefined
+                })
+            }
+
+            setProducts(temp)
+        },
+        [productList, filter],
+    )
+
+    useEffect(() => {
+        updateProducts()
+    }, [updateProducts])
+
     return (
         <Helmet title="Sản phẩm">
+        {
+            console.log(filter)
+        }
             <div className="catalog">
                 <div className="catalog__filter">
                     <div className="catalog__filter__widget">
@@ -25,6 +110,8 @@ const Catalog = () => {
                                     <div key={index} className="catalog__filter__widget__content__item">
                                         <CheckBox
                                             label={item.display}
+                                            onChange={(input) => filterSelect("CATEGORY", input.checked, item)}
+                                            checked={filter.category.includes(item.categorySlug)}
                                         />
                                     </div>
                                 ))
@@ -42,6 +129,8 @@ const Catalog = () => {
                                     <div key={index} className="catalog__filter__widget__content__item">
                                         <CheckBox
                                             label={item.display}
+                                            onChange={(input) => filterSelect("COLOR", input.checked, item)}
+                                            checked={filter.color.includes(item.color)}
                                         />
                                     </div>
                                 ))
@@ -59,10 +148,18 @@ const Catalog = () => {
                                     <div key={index} className="catalog__filter__widget__content__item">
                                         <CheckBox
                                             label={item.display}
+                                            onChange={(input) => filterSelect("SIZE", input.checked, item)}
+                                            checked={filter.size.includes(item.size)}
                                         />
                                     </div>
                                 ))
                             }
+                        </div>
+                    </div>
+
+                    <div className="catalog__filter__widget">
+                        <div className="catalog__filter__widget__content">
+                            <Button size="sm" onClick={() => clearFilter()}>xóa bộ lọc</Button>
                         </div>
                     </div>
                 </div>
@@ -74,7 +171,7 @@ const Catalog = () => {
                         gap={20}
                     >
                         {
-                            productData.getAllProducts().map((item, index) => (
+                            products.map((item, index) => (
                                 <ProductCard
                                     key={index}
                                     img01={item.image01}
